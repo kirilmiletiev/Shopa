65b3e61f4d60b8539806fb7c90bd1831e73626f5
KirilMiletiev <kirilmiletiev@gmail.com>
2018-12-26 02:04:23 +0200

    Add BaseModel.cs and Product.cs

---
+++ src/Data/Shopa.Data.Models/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopa.Data.Models
+{
+    public class BaseModel<TEntity>
+    {
+        public TEntity Id { get; set; } 
+    }
+}
---
+++ src/Data/Shopa.Data.Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopa.Data.Models
+{
+    public class Product : BaseModel<int>
+    {
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+        
+    }
+}

0d89d26c13cf48e7d9f978646bbd33f199fce0b4
KirilMiletiev <kirilmiletiev@gmail.com>
2018-12-30 13:12:40 +0200

    fix namespace

---
+++ src/Data/Shopa.Data.Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopa.Data.Models.Enums
+{
+   public class Review : BaseModel<int>
+    {
+        public ShopaUser User { get; set; }
+
+        public Product Product { get; set; }
+
+        public string Text { get; set; }
+
+        public Rating Rating { get; set; }
+    }
+}
